25df48e486c00b7bac40d58f1db335abea5c8cb8
eduardo-toste <duciochetti@gmail.com>
2026-04-03 23:47:53 -0300

    feat: add FindTaskByIdUseCase bean for retrieving tasks by ID

--- src/main/java/com/example/task_manager/configuration/CreateTaskConfig.java
+++ src/main/java/com/example/task_manager/configuration/CreateTaskConfig.java
@@ -2,8 +2,10 @@ package com.example.task_manager.configuration;
 
 import com.example.task_manager.application.mapper.TaskApplicationMapper;
 import com.example.task_manager.application.port.in.CreateTaskUseCase;
+import com.example.task_manager.application.port.in.FindTaskByIdUseCase;
 import com.example.task_manager.application.port.out.TaskRepositoryPort;
 import com.example.task_manager.application.usecase.CreateTaskService;
+import com.example.task_manager.application.usecase.FindTaskByIdService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +15,11 @@ public class CreateTaskConfig {
     @Bean
     public CreateTaskUseCase createTaskUseCase(TaskRepositoryPort taskRepositoryPort, TaskApplicationMapper taskApplicationMapper) {
         return new CreateTaskService(taskRepositoryPort,  taskApplicationMapper);
+    }
+
+    @Bean
+    public FindTaskByIdUseCase findTaskByIdUseCase(TaskRepositoryPort taskRepositoryPort, TaskApplicationMapper taskApplicationMapper) {
+        return new FindTaskByIdService(taskRepositoryPort,  taskApplicationMapper);
     }
 
 }

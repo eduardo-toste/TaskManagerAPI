d8743d4a82f85e4af70481b56a598082743b5103
eduardo-toste <duciochetti@gmail.com>
2026-04-04 16:56:00 -0300

    feat: add findTasks method to TaskRepositoryPort for paginated task retrieval

--- src/main/java/com/example/task_manager/application/port/out/TaskRepositoryPort.java
+++ src/main/java/com/example/task_manager/application/port/out/TaskRepositoryPort.java
@@ -1,12 +1,15 @@
 package com.example.task_manager.application.port.out;
 
 import com.example.task_manager.domain.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface TaskRepositoryPort {
 
     Task save(Task task);
+    Page<Task> findTasks(Pageable pageable);
     Optional<Task> findById(Long id);
 
 }

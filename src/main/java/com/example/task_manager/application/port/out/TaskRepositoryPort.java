2ce330c8e1d08f415dcf253836f9a30811c06bc9
eduardo-toste <duciochetti@gmail.com>
2026-04-03 23:47:34 -0300

    feat: add findById method to TaskRepositoryPort for retrieving tasks by ID

--- src/main/java/com/example/task_manager/application/port/out/TaskRepositoryPort.java
+++ src/main/java/com/example/task_manager/application/port/out/TaskRepositoryPort.java
@@ -2,8 +2,11 @@ package com.example.task_manager.application.port.out;
 
 import com.example.task_manager.domain.model.Task;
 
+import java.util.Optional;
+
 public interface TaskRepositoryPort {
 
     Task save(Task task);
+    Optional<Task> findById(Long id);
 
 }

3a5d9773207e58adbc54b3c6c7240480145b887a
eduardo-toste <duciochetti@gmail.com>
2026-04-04 16:56:37 -0300

    feat: add toResponse method in TaskWebMapper for converting paginated TaskOutput to TaskResponse

--- src/main/java/com/example/task_manager/adapter/in/web/mapper/TaskWebMapper.java
+++ src/main/java/com/example/task_manager/adapter/in/web/mapper/TaskWebMapper.java
@@ -4,6 +4,7 @@ import com.example.task_manager.adapter.in.web.request.CreateTaskRequest;
 import com.example.task_manager.adapter.in.web.response.TaskResponse;
 import com.example.task_manager.application.dto.CreateTaskCommand;
 import com.example.task_manager.application.dto.TaskOutput;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -23,6 +24,10 @@ public class TaskWebMapper {
                 taskOutput.description(),
                 taskOutput.status()
         );
+    }
+
+    public Page<TaskResponse> toResponse(Page<TaskOutput> taskOutputs) {
+        return taskOutputs.map(this::toResponse);
     }
 
 }

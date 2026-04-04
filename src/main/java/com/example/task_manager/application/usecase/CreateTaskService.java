186ef419cff59d03fc2c10ed68f5825e8c5bf68e
eduardo-toste <duciochetti@gmail.com>
2026-04-03 23:31:53 -0300

    feat: enhance CreateTaskService to use TaskApplicationMapper for output conversion

--- src/main/java/com/example/task_manager/application/usecase/CreateTaskService.java
+++ src/main/java/com/example/task_manager/application/usecase/CreateTaskService.java
@@ -2,6 +2,7 @@ package com.example.task_manager.application.usecase;
 
 import com.example.task_manager.application.dto.CreateTaskCommand;
 import com.example.task_manager.application.dto.TaskOutput;
+import com.example.task_manager.application.mapper.TaskApplicationMapper;
 import com.example.task_manager.application.port.in.CreateTaskUseCase;
 import com.example.task_manager.application.port.out.TaskRepositoryPort;
 import com.example.task_manager.domain.model.Task;
@@ -9,9 +10,11 @@ import com.example.task_manager.domain.model.Task;
 public class CreateTaskService implements CreateTaskUseCase {
 
     private final TaskRepositoryPort taskRepositoryPort;
+    private final TaskApplicationMapper taskApplicationMapper;
 
-    public CreateTaskService(TaskRepositoryPort taskRepositoryPort) {
+    public CreateTaskService(TaskRepositoryPort taskRepositoryPort,  TaskApplicationMapper taskApplicationMapper) {
         this.taskRepositoryPort = taskRepositoryPort;
+        this.taskApplicationMapper = taskApplicationMapper;
     }
 
     @Override
@@ -24,11 +27,6 @@ public class CreateTaskService implements CreateTaskUseCase {
 
         Task savedTask = taskRepositoryPort.save(task);
 
-        return new TaskOutput(
-                savedTask.getId(),
-                savedTask.getTitle(),
-                savedTask.getDescription(),
-                savedTask.getStatus()
-        );
+        return taskApplicationMapper.toTaskOutput(task);
     }
 }

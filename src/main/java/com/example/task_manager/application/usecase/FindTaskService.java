9a9340a3293321930d3c784f8b14b637c6da74de
eduardo-toste <duciochetti@gmail.com>
2026-04-04 16:56:05 -0300

    feat: implement findTasks method in FindTaskService for paginated task retrieval

--- src/main/java/com/example/task_manager/application/usecase/FindTaskService.java
+++ src/main/java/com/example/task_manager/application/usecase/FindTaskService.java
@@ -5,6 +5,8 @@ import com.example.task_manager.application.mapper.TaskApplicationMapper;
 import com.example.task_manager.application.port.in.FindTaskUseCase;
 import com.example.task_manager.application.port.out.TaskRepositoryPort;
 import com.example.task_manager.domain.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public class FindTaskService implements FindTaskUseCase {
 
@@ -14,6 +16,11 @@ public class FindTaskService implements FindTaskUseCase {
     public FindTaskService(TaskRepositoryPort taskRepositoryPort, TaskApplicationMapper taskApplicationMapper) {
         this.taskRepositoryPort = taskRepositoryPort;
         this.taskApplicationMapper = taskApplicationMapper;
+    }
+
+    @Override
+    public Page<TaskOutput> findTasks(Pageable pageable) {
+        return taskApplicationMapper.toTaskOutputPage(taskRepositoryPort.findTasks(pageable));
     }
 
     @Override

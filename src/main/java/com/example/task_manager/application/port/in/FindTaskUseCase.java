6569f3b69d16cedeee70278438e5155d6fa25223
eduardo-toste <duciochetti@gmail.com>
2026-04-04 16:55:55 -0300

    feat: add findTasks method to FindTaskUseCase for paginated task retrieval

--- src/main/java/com/example/task_manager/application/port/in/FindTaskUseCase.java
+++ src/main/java/com/example/task_manager/application/port/in/FindTaskUseCase.java
@@ -1,9 +1,12 @@
 package com.example.task_manager.application.port.in;
 
 import com.example.task_manager.application.dto.TaskOutput;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface FindTaskUseCase {
 
+    Page<TaskOutput> findTasks(Pageable pageable);
     TaskOutput getTaskById(Long id);
 
 }

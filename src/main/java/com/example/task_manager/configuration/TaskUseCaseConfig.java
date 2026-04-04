f8f7f6ce0d084ba4cc653afc6a688d283e2fc4b4
eduardo-toste <duciochetti@gmail.com>
2026-04-04 16:35:34 -0300

    refactor: rename CreateTaskConfig to TaskUseCaseConfig for clarity

--- src/main/java/com/example/task_manager/configuration/TaskUseCaseConfig.java
+++ src/main/java/com/example/task_manager/configuration/TaskUseCaseConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CreateTaskConfig {
+public class TaskUseCaseConfig {
 
     @Bean
     public CreateTaskUseCase createTaskUseCase(TaskRepositoryPort taskRepositoryPort, TaskApplicationMapper taskApplicationMapper) {
